1f50a80ac898823b480f303b96f93ad4bd239e38
CodeBarbarian <morten.haugstad@gmail.com>
2017-10-30 23:20:49 +0100

    Some minor changes on the minecraft server utilites. Still prototyping.

--- Modules/CBB-Gaming/Minecraft/MinecraftServerUtilities/MinecraftServerUtilities.psm1
+++ Modules/CBB-Gaming/Minecraft/MinecraftServerUtilities/MinecraftServerUtilities.psm1
@@ -1,22 +1,13 @@
-# THIS FILE IS CURRENTLY UNDER PROTOTYPING
-#
-#
-#
-#
-#
-#
-$Servers = @(
-    @{name=""; shortname=""; version="" baseuri=""},
-    @{name=""}
-)
+# Verbose Preference
+$VerbosePreference = 'continue'
 
 
-# This is representing the server.properties file
-$ServerConfig = @{
-
-
-}
-
+    
+# Which drive on your computer is the server located
+$ServerDrive = "C:"
+# Which Path is the server directory located
+$ServerPath  = "\temp\servers\minecraft"
+    
 Function Show-MCHeader {
     [cmdletbinding()] param () 
     "
@@ -26,17 +17,102 @@ Function Show-MCHeader {
     ██║╚██╔╝██║██║██║╚██╗██║██╔══╝  ██║     ██╔══██╗██╔══██║██╔══╝     ██║   
     ██║ ╚═╝ ██║██║██║ ╚████║███████╗╚██████╗██║  ██║██║  ██║██║        ██║   
     ╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝        ╚═╝   
-                                                                            
+                                                                                
     "
 }
+    
+Function Write-Event {
+    [cmdletbinding(SupportsShouldProcess=$true)]
+    [OutputType([void])]
+    param (
+        [string]$Message,
+        [string]$Type
+    )
+    
+    switch ($Type) {
+        'error' {}
+        'verbose' {
+            # Write Verbose message
+            Write-Verbose ("$($Message)")
+        }
+    }
+}
+    
+<#
+.SYNOPSIS
+    Get the default server directory
+.DESCRIPTION
+    Get the default server directory, which is based on the VARIABLE:$ServerDrive and VARIABLE:$ServerPath
+.LINK
+    https://github.com/CodeBarbarian/poshprofile/tree/master/Modules/CBB-Gaming/Minecraft/MinecraftServerUtilities
+.NOTES
+        
+    
+#>
+Function Get-DefaultServerDirectory {
+    [cmdletbinding(SupportsShouldProcess=$true)]
+    [OutputType([string])]
+    param ()
+        
+    $RealPath = (Join-Path $ServerDrive $ServerPath)  
+    
+    if ($PSCmdlet.ShouldProcess("$($RealPath)")) {
+        return $RealPath   
+    }
+}
+    
+    
+Function Get-Servers {
+    [cmdletbinding(SupportsShouldProcess, ConfirmImpact='medium')]
+    [OutputType([psobject])]
+    param (
+        [parameter(mandatory=$true)]
+        [string]$Path
+    )
+    
 
+    # Return Object
+    $ReturnObject = @()
+
+    # Test Path vs Create path
+    if ((Test-Path -Path $Path) -ne $true) {
+        Write-Event -Message ("No server directory found, creating it..") -Type 'Verbose'
+        $DefaultServerPath = Get-DefaultServerDirectory
+        New-Item -Path $DefaultServerPath -ItemType Directory | Out-Null
+        Write-Event -Message ("Directory Created => $($Path)")
+    }
+    
+    $Servers = Get-ChildItem -Path $Path 
+
+    # Exit early
+    if (($Servers | Measure-Object).Count -eq 0) {
+        return $ReturnObject = @{
+            name = "none"
+            size = 0
+        }
+    } 
+
+    ## TODO 
+    foreach ($Server in $Servers) {
+        $ReturnObject += New-Object PSObject -Property @{
+            name = $Server.name
+            size = $Server.length
+        }
+    }
+
+}
+    
 Function Install-Server {
     [cmdletbinding(SupportsShouldProcess=$true)]
+    [OutputType([void])]
     param (
         [parameter(Mandatory=$true)]
         $ServerType,
-        [parameter(Mandatory=$true)]
-        $ServerVersion
+        $ServerVersion = "latest" # 'latest' keyword will download the server which are defined as default
     )
+    
+    if ($PSCmdlet.ShouldProcess("Downloading server files...")) {
+        #
+    }
 }
-
+    
